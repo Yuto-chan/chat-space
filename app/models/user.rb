5430c1e28a15acabcb3e8fa084acec1a77b5985a
Yuto-chan <63442439+Yuto-chan@users.noreply.github.com>
2020-04-17 11:07:59 +0900

    edited until creating model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_many :groups_users
+  has_many :users, through: :groups_users
+
   validates :name, presence: true, uniqueness: true
 end
