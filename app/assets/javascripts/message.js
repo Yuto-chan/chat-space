f7164dadc9069f4306c72a94b6b35fffc8ab642f
Yuto-chan <63442439+Yuto-chan@users.noreply.github.com>
2020-04-23 19:40:34 +0900

    all done

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,10 +1,9 @@
 $(function(){
-  // var last_message_id = $('.chat-main__message-list__user-info:last').data("message-id");
 
     function buildHTML(message){
-      if (message.image.url) {
+      if (message.image) {
         var html = 
-          `<div class="chat-main__message-list__user-info">
+         `<div class="chat-main__message-list__user-info" data-message-id=${message.id}>
             <div class="chat-main__message-list__user-info__name">
               ${message.user_name}
               <div class="chat-main__message-list__user-info__day">
@@ -14,14 +13,14 @@ $(function(){
             <div class="chat-main__message-list__user-text">
               <p class="message-list__user-text">
                 ${message.body}
-                ${message.image}
               </p>
+              <img src=${message.image}>
             </div>
           </div>`
         return html;
       } else {
         var html = 
-          `<div class="chat-main__message-list__user-info">
+          `<div class="chat-main__message-list__user-info" data-message-id=${message.id}>
             <div class="chat-main__message-list__user-info__name">
               ${message.user_name}
               <div class="chat-main__message-list__user-info__day">
@@ -67,6 +66,7 @@ $(function(){
   var reloadMessages = function() {
     //カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
     var last_message_id = $('.chat-main__message-list__user-info:last').data("message-id");
+   
     $.ajax({
       //ルーティングで設定した通り/groups/id番号/api/messagesとなるよう文字列を書く
       url: "api/messages",
@@ -77,10 +77,23 @@ $(function(){
       data: {id: last_message_id}
     })
     .done(function(messages) {
-      console.log('success');
+      if (messages.length !== 0) {
+        //追加するHTMLの入れ物を作る
+        var insertHTML = '';
+        //配列messagesの中身一つ一つを取り出し、HTMLに変換したものを入れ物に足し合わせる
+        $.each(messages, function(i, message) {
+          insertHTML += buildHTML(message)
+        });
+        //メッセージが入ったHTMLに、入れ物ごと追加
+        $('.chat-main__message-list').append(insertHTML);   
+        $('.chat-main__message-list').animate({ scrollTop: $('.chat-main__message-list')[0].scrollHeight});
+      }
     })
     .fail(function() {
       alert('error');
     });
-  };
+  };    
+  if (document.location.href.match(/\/groups\/\d+\/messages/)) {
+    setInterval(reloadMessages, 7000);
+  } 
 });
